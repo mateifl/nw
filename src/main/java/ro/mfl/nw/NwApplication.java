a0ff70a174bca999904b29288775b3c073fb972e
mateifl <mateifl@yahoo.com>
2020-08-28 16:23:46 +0300

    Initial version

---
+++ src/main/java/ro/mfl/nw/NwApplication.java
@@ -0,0 +1,13 @@
+package ro.mfl.nw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mfl/nw/NwApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mfl.nw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
